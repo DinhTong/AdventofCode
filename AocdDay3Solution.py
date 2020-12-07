0a5d2e70490bddbc4ddde8e7885aaa3028c2d11f
DinhTong <66277033+DinhTong@users.noreply.github.com>
2020-12-06 22:49:25 -0600

    Scrape day 3 input

---
+++ AocdDay3Solution.py
@@ -0,0 +1,10 @@
+import urllib3
+
+# open a connection to a URL using urllib
+http = urllib3.PoolManager()
+webUrl  = http.request('GET', 'https://raw.githubusercontent.com/DinhTong/AdventofCode/main/AocdDay3Input.txt')
+
+# read the data from the URL and print it
+data = webUrl.data.splitlines()
+
+print data[:10]
