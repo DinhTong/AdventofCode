d47e5818411235b30824782a2fb7065a1cc626ba
DinhTong <66277033+DinhTong@users.noreply.github.com>
2020-12-07 23:04:12 -0600

    Upload day 3 Part 1

--- AocdDay3Solution.py
+++ AocdDay3Solution.py
@@ -7,4 +7,22 @@ webUrl  = http.request('GET', 'https://raw.githubusercontent.com/DinhTong/Advent
 # read the data from the URL and print it
 data = webUrl.data.splitlines()
 
-print data[:10]
+# print len(data[0])
+
+
+#Begin for loop
+posCalulation = lambda x, y : x + 3 - y if x + 3 > y else x + 3 #Calculate right 3 down 1
+pos = 1
+result1 = 0
+
+for i in data:
+    pos = pos - 1
+    lenData = len(i)
+    if i[pos] == '#':
+        result1 = result1 + 1
+    pos = posCalulation (pos + 1, lenData)
+
+print result1
+
+
+
