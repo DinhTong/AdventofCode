be534cce4994c2d5b472c6f412e3f4f563bec72d
DinhTong <66277033+DinhTong@users.noreply.github.com>
2020-12-13 11:09:06 -0600

    upload day 6 part 1 solution

--- AocdDay6Solution.py
+++ AocdDay6Solution.py
@@ -1,12 +1,21 @@
 import urllib3
+from collections import OrderedDict
 
 # open a connection to a URL using urllib
 http = urllib3.PoolManager()
 webUrl  = http.request('GET', 'https://raw.githubusercontent.com/DinhTong/AdventofCode/main/AocdDay6Input.txt')
 
 # read the data from the URL and print it
-data = webUrl.data.splitlines()
-
-print (data)
+data = webUrl.data.split("\n\n")
 
 #Day 6 part 1
+#Remove duplicate chars within a string
+result1 = 0
+def removeDupWithOrder(str):
+    return "".join(OrderedDict.fromkeys(str))
+
+for i in data:
+    i = i.replace("\n","")
+    result1 = result1 + len(removeDupWithOrder(i))
+
+print result1
--- test.py
+++ test.py
@@ -1,17 +1,15 @@
-x = input('Enter test number : ')
+from collections import OrderedDict
 
-print (x % 2)
+#x = input('Enter test number : ')
+x = "eao\nwoa\nao\noa"
 
-Row : 76 - Seat : 0 - ID : 608 - 
-0 - 127
-B:64 - 127  
-F:64 - 95
-F:64 - 79
-B:72 - 79
-B:76 - 79
-F:76 - 77
-F:76
-0 - 7
-L:0 - 3
-L:0 - 1
-L:0
+dataInput = x.replace("\n", "")
+
+def removeDupWithOrder(str):
+    return "".join(OrderedDict.fromkeys(str))
+
+if __name__ == "__main__":
+    print ("Original String : ", dataInput)
+    print ("Dedupped String : ", removeDupWithOrder(dataInput))
+
+    
