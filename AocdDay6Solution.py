76ff9642fe04a7de925ced19c5a888f010166f9e
DinhTong <66277033+DinhTong@users.noreply.github.com>
2020-12-13 13:48:59 -0600

    Upload Day 6 part 2 solution

--- AocdDay6Solution.py
+++ AocdDay6Solution.py
@@ -1,5 +1,6 @@
 import urllib3
 from collections import OrderedDict
+from collections import Counter
 
 # open a connection to a URL using urllib
 http = urllib3.PoolManager()
@@ -8,14 +9,39 @@ webUrl  = http.request('GET', 'https://raw.githubusercontent.com/DinhTong/Advent
 # read the data from the URL and print it
 data = webUrl.data.split("\n\n")
 
-#Day 6 part 1
-#Remove duplicate chars within a string
-result1 = 0
+#Remove duplicate characters within a string
 def removeDupWithOrder(str):
     return "".join(OrderedDict.fromkeys(str))
 
+#Count questions that everyone answered "yes"
+#Count how many times a character appear in a string
+def countQuestionEveryoneAnsweredYes (str):
+    r = 0
+    pCount = Counter(str) 
+    count = pCount['\n'] + 1 #Count how many people in the group
+    allAns = str.replace("\n","") #Combine all answers into one string
+    cCount = Counter(allAns) #Function to count duplicate
+    simAns = removeDupWithOrder(allAns) 
+    for i in simAns:
+        chk = cCount[i]
+        if chk == count:
+            r = r + 1
+    return r
+
+#Day 6 part 1
+#Remove duplicate chars within a string
+result1 = 0
 for i in data:
     i = i.replace("\n","")
     result1 = result1 + len(removeDupWithOrder(i))
+print "Day 6 part 1 result: ", result1
 
-print result1
+#Day 6 part 2
+result2 = []
+for i in data:
+    r = countQuestionEveryoneAnsweredYes(i)
+    result2.append(r)
+
+print "Day 6 part 2 result: ", sum(result2)
+
+
--- test.py
+++ test.py
@@ -1,15 +1,45 @@
 from collections import OrderedDict
+from collections import Counter
 
 #x = input('Enter test number : ')
 x = "eao\nwoa\nao\noa"
 
-dataInput = x.replace("\n", "")
+# dataInput = x.replace("\n", "")
 
 def removeDupWithOrder(str):
     return "".join(OrderedDict.fromkeys(str))
 
-if __name__ == "__main__":
-    print ("Original String : ", dataInput)
-    print ("Dedupped String : ", removeDupWithOrder(dataInput))
+#Count number of people in group
+result1 = 0
+pCount = Counter(x)
+count = pCount['\n'] + 1
+allAns = x.replace("\n","")
+cCount = Counter(allAns)
+simAns = removeDupWithOrder(allAns)
 
-    
+for i in simAns:
+    chk = cCount[i]
+    if chk == count:
+        result1 = result1 + 1
+
+#Count questions that everyone answered "yes"
+#Count how many times a character appear in a string
+def countQuestionEveryoneAnsweredYes (str):
+    r = 0
+    pCount = Counter(x) 
+    count = pCount['\n'] + 1 #Count how many people in the group
+    allAns = x.replace("\n","") #Combine all answers into one string
+    cCount = Counter(allAns) #Function to count duplicate
+    simAns = removeDupWithOrder(allAns) 
+    for i in simAns:
+        chk = cCount[i]
+        if chk == count:
+            r = r + 1
+    return r
+
+result = countQuestionEveryoneAnsweredYes(x)
+
+print result
+print result1
+
+
