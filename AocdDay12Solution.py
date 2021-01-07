49f6e36d5b7b8cf498c8d4e74d8a9c1576eca021
DinhTong <tongxuandinh@yahoo.com>
2021-01-06 21:39:39 -0600

    Day 12 part 2 solution

--- AocdDay12Solution.py
+++ AocdDay12Solution.py
@@ -23,51 +23,39 @@ right = lambda currentDirection, turnDegree: currentDirection - (turnDegree/90) 
 aInstr = lambda string: string[0]
 aValue = lambda string: string[1:len(string)]
 
-# want to find a better way to build this 2 way dict (Oldway)
-def dirConvert (val):
-    if val == 1:
-        return 'E'
-    elif val == 2:
-        return 'N'
-    elif val == 3:
-        return 'W'
-    elif val == 4:
-        return 'S'
-    elif val == 'E':
-        return 1
-    elif val == 'N':
-        return 2
-    elif val == 'W':
-        return 3
-    elif val == 'S':
-        return 4
-
 # this is a better way to do the 2 way convert. I can expanse the crosswalk.
 crossWalk = ['E-1','N-2','W-3','S-4']
 # d2v = direction to value. This translates direction initial to value.
 # v2d = value to direction. This translates value to direction initial.
+
 def directionTranslator (x, y):
-    if x == 'd2v':
-        for i in crossWalk:
-            temp = i.split('-')
-            val1 = temp[0]
-            val2 = temp[1]
-            if y == val1:
-                return val2
-    elif x == 'v2d':
-        for i in crossWalk:
-            temp = i.split('-')
-            val1 = temp[0]
-            val2 = temp[1]
-            if str(y) == val2:
-                return val1
+    for i in crossWalk:
+        temp = i.split('-')
+        val1 = temp[0]
+        val2 = temp[1]
+        if x == 'd2v' and y == val1:
+            return val2
+        elif x == 'v2d' and str(y) == val2:
+            return val1
 
-
+#Turn direction are either left (L) or right (R)
+#Current direction must be first initial
+#Turn degree can only be devisible by 90 (90, 180, 270, 360...)
+def directionChanging (turnDirection, currentDirection, turnDegree):
+    if turnDirection == 'L':
+        currentDirection = int(directionTranslator('d2v',currentDirection)) #translate current direction to direction value
+        currentDirection = left (currentDirection, turnDegree) #Calculate new direction using giving information
+        currentDirection = directionTranslator('v2d',currentDirection) #translate new direction value to new direction initial
+        return currentDirection
+    elif turnDirection == 'R':
+        currentDirection = int(directionTranslator('d2v',currentDirection)) #translate current direction to direction value
+        currentDirection = right (currentDirection, turnDegree) #Calculate new direction using giving information
+        currentDirection = directionTranslator('v2d',currentDirection) #translate new direction value to new direction initial
+        return currentDirection
 
 curDir = 'E' #Current facing direction. Starting direction is East
 north = 0 #Starting position North - 0, negative north value = positive south value
 east = 0 #Starting position East - 0, negative east value = positive west value
-
 
 # Day 12 part 1
 
@@ -80,19 +68,104 @@ for i in rawData:
         east = east + int(aValue(i))
     elif aInstr(i) == 'W'or (aInstr(i) == 'F' and curDir == 'W'):
         east = east - int(aValue(i))
-    elif aInstr(i) == 'L':
-        curDir = int(directionTranslator('d2v',curDir)) #translate current direction to direction value
-        curDir = left (curDir, int( aValue(i))) #Calculate new direction using giving information
-        curDir = directionTranslator('v2d',curDir) #translate new direction value to new direction initial
-    elif aInstr(i) == 'R':
-        curDir = int(directionTranslator('d2v',curDir)) #translate current direction to direction value
-        curDir = right (curDir, int( aValue(i))) #Calculate new direction using giving information
-        curDir = directionTranslator('v2d',curDir) #translate new direction value to new direction initial
+    elif aInstr(i) == 'L' or aInstr(i) == 'R':
+        curDir = directionChanging(aInstr(i), curDir, int(aValue(i)))
 
 result1 = abs(north) + abs(east)
 
 print ('Day 12 part 1 result : ' + str(result1))
 
 # Day 12 part 2
+result2 = 0
+waypoint = 'E-10-N-1'#Starting waypoint values: 10 units east and 1 unit north
+shiplocation = 'E-0-N-0' 
+curDir = 'E' #Current facing direction. Starting direction is East
 
+for i in rawData:
+    temp = waypoint.split('-')
+    wp1 = temp[0]
+    wp2 = int(temp[1])
+    wp3 = temp[2]
+    wp4 = int(temp[3])
+    temp = shiplocation.split('-')
+    sl1 = temp[0]
+    sl2 = int(temp[1])
+    sl3 = temp[2]
+    sl4 = int(temp[3]) 
+    action = aInstr(i)
+    actionValue = int(aValue(i))
+
+    if action == 'L' or action == 'R':
+        wp1 = directionChanging(action, wp1, actionValue)
+        wp3 = directionChanging(action, wp3, actionValue)
+    #Ship first coordinate can only change from east to west and vice versa.
+    #if waypoint FIRST coordinate store East and West value. Then match waypoint FIRST coordinate value to ship FIRST coordinate value.
+    elif action == 'F' and (wp1 == 'E' or directionChanging('L', wp1, 180) == 'E'): 
+        if wp1 == sl1 and wp3 == sl3:
+            sl2 = sl2 + (wp2 * actionValue)
+            sl4 = sl4 + (wp4 * actionValue)
+        elif wp1 == directionChanging('L', sl1, 180) and wp3 == sl3:
+            sl2 = sl2 - (wp2 * actionValue)
+            sl4 = sl4 + (wp4 * actionValue)
+        elif wp1 == sl1 and wp3 == directionChanging('L', sl3, 180):
+            sl2 = sl2 + (wp2 * actionValue)
+            sl4 = sl4 - (wp4 * actionValue)
+        elif wp1 == directionChanging('L', sl1, 180) and wp3 == directionChanging('L', sl3, 180):
+            sl2 = sl2 - (wp2 * actionValue)
+            sl4 = sl4 - (wp4 * actionValue)
+    #if waypoint FIRST coordinate does not store East and West value. Then match waypoint FIRST coordinate value to ship SECOND coordinate value
+    elif action == 'F' and (wp1 != 'E' or directionChanging('L', wp1, 180) != 'E'): 
+        if wp1 == sl3 and wp3 == sl1:
+            sl2 = sl2 + (wp4 * actionValue)
+            sl4 = sl4 + (wp2 * actionValue)
+        elif wp1 == directionChanging('L', sl3, 180) and wp3 == sl1:
+            sl2 = sl2 + (wp4 * actionValue)
+            sl4 = sl4 - (wp2 * actionValue)
+        elif wp1 == sl3 and wp3 == directionChanging('L', sl1, 180):
+            sl2 = sl2 - (wp4 * actionValue)
+            sl4 = sl4 + (wp2 * actionValue)
+        elif wp1 == directionChanging('L', sl3, 180) and wp3 == directionChanging('L', sl1, 180):
+            sl2 = sl2 - (wp4 * actionValue)
+            sl4 = sl4 - (wp2 * actionValue)
+    elif action == wp1:
+        wp2 = wp2 + actionValue
+    elif directionChanging('L', action, 180) == wp1:
+        wp2 = wp2 - actionValue
+    elif action == wp3:
+        wp4 = wp4 + actionValue
+    elif directionChanging('L', action, 180) == wp3:
+        wp4 = wp4 - actionValue
+
+
+    #Updating waypoint
+    if wp2 < 0: #If value < 0 then switch to oppsite direction
+        wp1 = directionChanging('L', wp1, 180)
+        wp2 = wp2 * -1
+    
+    if wp4 < 0:
+        wp3 = directionChanging('L', wp3, 180)
+        wp4 = wp4 * -1
+    waypoint = wp1 + '-' + str(wp2) + '-' + wp3 + '-' + str(wp4)
+    #print ('Waypoint : ' + waypoint)
+
+    #Updating ship location
+    if sl2 < 0:
+        sl1 = directionChanging('L', sl1, 180)
+        sl2 = sl2 * -1
+
+    if sl4 < 0:
+        sl3 = directionChanging('L', sl3, 180)
+        sl4 = sl4 * -1
+    shiplocation = sl1 + '-' + str(sl2) + '-' + sl3 + '-' + str(sl4)
+    #print ('Ship location : ' + shiplocation)
+
+
+temp = shiplocation.split('-')
+sl1 = temp[0]
+sl2 = int(temp[1])
+sl3 = temp[2]
+sl4 = int(temp[3]) 
+
+result2 = sl2 + sl4
+print ('Day 12 part 2 result : ' + str(result2))
 
